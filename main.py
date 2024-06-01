0bb8d2ee56ad3ab5c915e229588acdbfe0174288
Renato <renato92.hora@gmail.com>
2024-06-01 14:30:45 -0300

    Edit added

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 todos = []
 
 while True:
-    user_action = input("Type add, show or exit")
+    user_action = input("Type add, show, edit or exit ")
+    user_action = user_action.strip()
 
     match user_action:
         case 'add':
@@ -10,7 +11,13 @@ while True:
         case 'show':
             for item in todos:
                 print(item)
+        case 'edit':
+            number = int(input("Number of the todo to edit: "))
+            number = number - 1
+            new_todo = input("Enter new todo: ")
+            todos[number] = new_todo
         case 'exit':
             break
+
 
 print("Bye!")
